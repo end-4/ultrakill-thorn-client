125c3893e0bdc4cda5fbb31ff9fcb3a2173aa8ff
end-4 <97237370+end-4@users.noreply.github.com>
2026-07-13 13:33:08 +0700

    fix cheat manager warning titlescreen

--- ThornClient/Managers/CheatManager.cs
+++ ThornClient/Managers/CheatManager.cs
@@ -13,11 +13,12 @@ public static class CheatManager {
     private static string _hexColor = "#44ff45";
 
     static CheatManager() {
-        SceneManager.sceneLoaded += ResetCheatiness;
+        SceneManager.sceneLoaded += ResetCheatinessAndUpdate;
     }
 
-    private static void ResetCheatiness(Scene scene, LoadSceneMode mode) {
+    private static void ResetCheatinessAndUpdate(Scene scene, LoadSceneMode mode) {
         ResetCheatiness();
+        UpdateCheatiness();
     }
 
     private static void ResetCheatiness() {
@@ -31,18 +32,25 @@ public static class CheatManager {
     }
 
     public static void UpdateCheatiness() {
+        // Plugin.Log.LogInfo(
+        //     $"Checking in scene '{SceneHelper.CurrentScene}', length = {SceneHelper.CurrentScene?.Length ?? 0}");
+        if (SceneHelper.CurrentScene == "Main Menu" || SceneHelper.CurrentScene == "Bootstrap" ||
+            !((SceneHelper.CurrentScene?.Length ?? 0) > 0)) return;
         bool lastCheaty = _cheating;
         var cheaties = GetActiveCheatyModules();
         if (cheaties.Count > 0) {
             _cheating = true;
             LeaderboardHelper.DisableLeaderboards();
+            // Plugin.Log.LogInfo("Cheaty!");
         }
 
         if (!lastCheaty && _cheating) {
             NotificationSystem.NotifySend(
                 $"Thorn: <color={_hexColor}>Cheats</color>",
                 "Leaderboard disabled for this run\n" +
-                string.Join("\n", cheaties.Select(module => $"- <color={_hexColor}><u>{module.Name}</u></color>: {module.CheatReason}")),
+                string.Join("\n",
+                    cheaties.Select(
+                        module => $"- <color={_hexColor}><u>{module.Name}</u></color>: {module.CheatReason}")),
                 iconFilePath: Plugin.PluginIconPath
             );
         }
