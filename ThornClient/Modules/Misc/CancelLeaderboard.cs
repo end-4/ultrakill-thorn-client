c8303a62d71dff0442f74de98ceab0112e738bb1
end-4 <97237370+end-4@users.noreply.github.com>
2026-07-18 13:23:29 +0700

    add cancel leaderboard module

---
+++ ThornClient/Modules/Misc/CancelLeaderboard.cs
@@ -0,0 +1,16 @@
+﻿using ThornClient.Core;
+using ThornClient.Managers;
+
+namespace ThornClient.Modules.Misc;
+
+public class CancelLeaderboard : Module {
+    public override string IconName => "no_trophy";
+    public CancelLeaderboard() : base("thorn.cancelLeaderboard", "Cancel Leaderboard", "Voluntarily exclude scores from the leaderboard. Useful when you're using cheaty mods that don't do this themselves", ModuleCategory.Misc) {
+    }
+
+    public override string CheatReason => IsEnabled ? "Obviously" : "";
+
+    protected override void OnEnable() {
+        CheatManager.UpdateCheatiness();
+    }
+}
