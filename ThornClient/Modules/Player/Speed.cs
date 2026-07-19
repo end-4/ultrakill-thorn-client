c849a67a3e67d79a8fc594f272f58b083f03eaaf
end-4 <97237370+end-4@users.noreply.github.com>
2026-07-19 16:46:49 +0700

    add speed module

---
+++ ThornClient/Modules/Player/Speed.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using ThornClient.Core;
+
+namespace ThornClient.Modules.Player;
+
+public class EnemyTracers : Module {
+    private static readonly float BaseWalkSpeed = 750;
+    public Setting<float> SpeedMultiplier { get; }
+    public override string IconName => "speed";
+
+    public EnemyTracers() : base("thorn.speed", "Speed", "Adjusts your speed",
+        ModuleCategory.Player) {
+        SpeedMultiplier = RegisterSetting("speedMultiplier", "Speed multiplier", "How much to scale the speed", 2f);
+    }
+
+    public override string? CheatReason => IsEnabled ? "Adds crazy movement" : "";
+
+    protected override void OnEnable() {
+        UpdateSpeed(SpeedMultiplier.Value);
+        SpeedMultiplier.OnValueChanged += UpdateSpeed;
+    }
+
+    protected override void OnDisable() {
+        SpeedMultiplier.OnValueChanged -= UpdateSpeed;
+        UpdateSpeed(1f);
+    }
+
+    private void UpdateSpeed(float multiplier) {
+        var nm = NewMovement.Instance;
+        if (nm == null) return;
+        nm.walkSpeed = BaseWalkSpeed * multiplier;
+    }
+}
