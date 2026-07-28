686a810e5983661be78fc8325dce94c6f9d14d6f
end-4 <97237370+end-4@users.noreply.github.com>
2026-07-28 20:27:58 +0700

    make value scrub cursor show also when dragging-but-left-area

--- ThornClient/System/ClickGUIComponents/ValueScrubController.cs
+++ ThornClient/System/ClickGUIComponents/ValueScrubController.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using ThornClient.Core;
+using ThornClient.Managers;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace ThornClient.System.ClickGUIComponents;
 
-public class ValueScrubController : MonoBehaviour, IDragHandler, IBeginDragHandler {
+public class ValueScrubController : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler,
+    IPointerEnterHandler,
+    IPointerExitHandler {
     public float scaleFactor = 0.1f;
-
+    private Vector2 _hotspot = new Vector2(48, 48);
     private Vector2 _cumulatedDelta;
+
+    private bool _dragging = false;
+    private bool _hovering = false;
 
     /// <summary>
     /// The event emitted when dragged. This is the TOTAL value since drag starts
@@ -21,6 +27,7 @@ public class ValueScrubController : MonoBehaviour, IDragHandler, IBeginDragHandl
     public event Action OnScrubStart;
 
     public void OnDrag(PointerEventData eventData) {
+        // TODO add a snackbar/toast to Notiffy then use it for vertical sensitivity adjustment
         _cumulatedDelta += eventData.delta * scaleFactor;
         OnValueScrub?.Invoke(_cumulatedDelta.x);
     }
@@ -28,5 +35,32 @@ public class ValueScrubController : MonoBehaviour, IDragHandler, IBeginDragHandl
     public void OnBeginDrag(PointerEventData eventData) {
         _cumulatedDelta = Vector2.zero;
         OnScrubStart?.Invoke();
+        _dragging = true;
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        _cumulatedDelta = Vector2.zero;
+        OnScrubStart?.Invoke();
+        _dragging = false;
+        UpdateCursor();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        _hovering = true;
+        UpdateCursor();
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        _hovering = false;
+        UpdateCursor();
+    }
+
+    private void UpdateCursor() {
+        if (_hovering || _dragging) {
+            Cursor.SetCursor(AssetManager.Get<Texture2D>(ClickGUI.BundleKey, "move_horizontal_cursor"), _hotspot,
+                CursorMode.Auto);
+        } else {
+            Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        }
     }
 }
