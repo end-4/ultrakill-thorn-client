8d8e77207c86c1e5f9b2f3ce24925417ffcf0a6f
end-4 <97237370+end-4@users.noreply.github.com>
2026-08-19 14:04:04 +0700

    fix content size fitter null error in boundevaluehudmodule

--- ThornClient/HUD/BoundedValueHudModule.cs
+++ ThornClient/HUD/BoundedValueHudModule.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using NukeLib.UI;
 using NukeLib.Utils;
 using ThornClient.Core.ConfigurableElements;
+using ThornClient.Core.UI;
 using ThornClient.HUD.HUDComponents;
 using ThornClient.Managers;
 using UnityEngine;
@@ -156,7 +157,8 @@ public abstract class BoundedValueHudModule : FramedHudModule {
     /// <param name="displayIcon">The icon shown on the HUD element</param>
     /// <param name="decimalPlaces">The decimal places to show on the value text</param>
     public BoundedValueHudModule(string guid, string name, string description, float bound = 1, string displayName = "",
-        Sprite? displayIcon = null, int decimalPlaces = 1, Color? defaultValueColor = null, Color? defaultSoftBoundColor = null) : base(guid,
+        Sprite? displayIcon = null, int decimalPlaces = 1, Color? defaultValueColor = null,
+        Color? defaultSoftBoundColor = null) : base(guid,
         name, description) {
         Style = CreateSetting("indicatorStyle", "Indicator Style", "The style to present the value",
             IndicatorStyle.Progress);
@@ -233,7 +235,9 @@ public abstract class BoundedValueHudModule : FramedHudModule {
         var fitterComp = Wrapper.GetComponent<SingularScalableContentSizeFitter>();
         if (fitterComp != null) {
             fitterComp.enabled = false;
-            ExecutionUtils.RunNextFrame(() => { fitterComp.enabled = true; });
+            ExecutionUtils.RunNextFrame(() => {
+                if (fitterComp != null) fitterComp.enabled = true;
+            });
         }
     }
 }
