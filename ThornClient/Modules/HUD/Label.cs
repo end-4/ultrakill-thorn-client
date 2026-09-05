bb51015844515e4f7a27a5538ce1ae39b0c349d4
Lucas11 <63660708+lucas11222@users.noreply.github.com>
2026-09-05 13:45:11 +0200

    finished adding labels and restart shortcuts!

---
+++ ThornClient/Modules/HUD/Label.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using ThornClient.Core.ConfigurableElements;
+using ThornClient.Managers;
+using ThornClient.System;
+using UnityEngine;
+using ThornClient.HUD;
+
+namespace ThornClient.Modules.HUD;
+
+/// <summary>
+/// Module that shows FPS
+/// </summary>
+public class Label : ContinuousCumulatedStatHudModule {
+    /// <summary>
+    /// Setting: whether to show "FPS" text on the HUD element
+    /// </summary>
+    public Setting<string> CustomText;
+
+    /// <summary>
+    /// Icon of the module
+    /// </summary>
+    public override Sprite Icon => AssetManager.Get<Sprite>(ClickGUI.BundleKey, "cube");
+
+    /// <summary>
+    /// Tags for search
+    /// </summary>
+    public override string[] Tags => ["bonus"];
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public Label() : base("thorn.label", "Label", "Shows a custom text") {
+        CustomText = CreateSetting("customText", "Text",
+            "Adds an text to the HUD", "placeholder (change this in settings!)");
+    }
+
+    /// <inheritdoc />
+    protected override float CollectUpdate() {
+        return 1f;
+    }
+
+    /// <inheritdoc />
+    protected override string FormatStat(float value) {
+        return CustomText.Value;
+    }
+}
---
+++ ThornClient/Modules/Utility/RestartShortcut.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using ThornClient.Core;
+using ThornClient.Core.DataTypes;
+using ThornClient.Core.ConfigurableElements;
+using ThornClient.Managers;
+using ThornClient.System;
+
+namespace ThornClient.Modules.Utility;
+
+/// <summary>
+/// Use this shortcut to restart your mission. Good for speedruns.
+/// </summary>
+public class RestartShortcut : Module {
+    /// <summary>
+    /// The FOV that is applied when zooming
+    /// </summary>
+    public Setting<Keybind> RestartKeybind { get; }
+    public Setting<Keybind> RestartCheckpoint { get; }
+
+    /// <summary>
+    /// Icon of the module
+    /// </summary>
+    public override Sprite Icon => AssetManager.Get<Sprite>(ClickGUI.BundleKey, "dash");
+
+    /// <summary>
+    /// Tags for search
+    /// </summary>
+    public override string[] Tags => ["utility", "speedrun"];
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public RestartShortcut() : base("thorn.extraKeybinds", "Extra keybinds",
+        "Some useful keybinds for (almost) everything! (right now its only speedruns things)", ModuleCategory.Utility) {
+        RestartKeybind = CreateSetting("restartMission", "Restart mission.", "yo what do you think it does :sob:",
+            new Keybind(KeyCode.H));
+        RestartKeybind.OnPress += () => OptionsManager.Instance?.RestartMission();
+
+        RestartCheckpoint = CreateSetting("restartToCheckpoint", "Go to checkpoint.", "yo what do you think it does :sob:",
+            new Keybind(KeyCode.B));
+        RestartCheckpoint.OnPress += () => OptionsManager.Instance?.RestartCheckpoint();
+    }
+}
