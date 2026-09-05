8a11558e4f485f3da75e212cfeeaa61b272c2ff9
Lucas11 <63660708+lucas11222@users.noreply.github.com>
2026-09-05 16:52:26 +0200

    Add Extra Binds and Labels (#6)
    * finished adding labels and restart shortcuts!
    
    * update the changelog
    
    * rename!
    
    * fixing some stuff
    
    * making some description better
    
    * rename
    
    * make it so it dosent do it every time
    
    * Update ExtraBinds.cs

---
+++ ThornClient/Modules/HUD/CustomLabel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using ThornClient.Core.ConfigurableElements;
+using ThornClient.Managers;
+using ThornClient.System;
+using UnityEngine;
+using ThornClient.HUD;
+
+namespace ThornClient.Modules.HUD;
+
+/// <summary>
+/// Module that shows a custom text.
+/// </summary>
+public class CustomLabel : TextHudModule {
+    /// <summary>
+    /// Setting: The custom text that will appear on the HUD
+    /// </summary>
+    public Setting<string> CustomText;
+
+    /// <summary>
+    /// Icon of the module
+    /// </summary>
+    public override Sprite Icon => AssetManager.Get<Sprite>(ClickGUI.BundleKey, "cube");
+
+    /// <summary>
+    /// Tags for search
+    /// </summary>
+    public override string[] Tags => ["text"];
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public CustomLabel() : base("thorn.customLabel", "Custom Label", "Shows a custom text") {
+        CustomText = CreateSetting("customText", "Text",
+            "Adds an text to the HUD", "placeholder (change this in settings!)");
+        CustomText.OnChanged += ChangeText;
+    }
+
+    /// <summary>
+    /// Changes the value of the label.
+    /// </summary>
+    public void ChangeText() {
+        Text = CustomText.Value;
+    }
+
+    protected override void OnHudModuleEnable() {
+        ChangeText();
+    }
+}
---
+++ ThornClient/Modules/Utility/ExtraBinds.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using ThornClient.Core;
+using ThornClient.Core.DataTypes;
+using ThornClient.Core.ConfigurableElements;
+using ThornClient.Managers;
+using ThornClient.System;
+
+namespace ThornClient.Modules.Utility;
+
+/// <summary>
+/// Use this shortcut to restart your mission. Good for speedruns.
+/// </summary>
+public class ExtraBinds : Module {
+    /// <summary>
+    /// The settings for the keybinds
+    /// </summary>
+    public Setting<Keybind> RestartKeybind { get; }
+    public Setting<Keybind> CheckpointKeybind { get; }
+
+    /// <summary>
+    /// Icon of the module
+    /// </summary>
+    public override Sprite Icon => AssetManager.Get<Sprite>(ClickGUI.BundleKey, "plus");
+
+    /// <summary>
+    /// Tags for search
+    /// </summary>
+    public override string[] Tags => ["utility", "speedrun"];
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public ExtraBinds() : base("thorn.extraBinds", "Extra Binds",
+        "Some useful keybinds for (almost) everything", ModuleCategory.Utility) {
+        RestartKeybind = CreateSetting("restartMission", "Restart mission", "Keybind to restart mission",
+            new Keybind(KeyCode.None));
+        RestartKeybind.OnPress += () => OptionsManager.Instance?.RestartMission();
+
+        CheckpointKeybind = CreateSetting("restartToCheckpoint", "Checkpoint Keybind", "Goes to the latest checkpoint",
+            new Keybind(KeyCode.None));
+        CheckpointKeybind.OnPress += () => OptionsManager.Instance?.RestartCheckpoint();
+    }
+}
